2989eda3734e9f39603a76256b27a7a40b699b19
WaWa138 <49987003+WaWa138@users.noreply.github.com>
2021-06-17 02:07:54 +0800

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,102 +1,152 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
+void main() {
+  runApp(MyApp());
 }
 
-class _MyAppState extends State<MyApp> {
-    @override
-
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      initialIndex: 1,
-      length: 3,
-      child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.blueGrey.shade500,
-          title: Text('TabBar Widget'),
-          bottom: TabBar(
-            indicatorColor: Colors.lime,
-            indicatorWeight: 5.0,
-            labelColor: Colors.white,
-            labelPadding: EdgeInsets.only(top: 10.0),
-            unselectedLabelColor: Colors.grey,
-            tabs: [
-              Tab(
-                text: 'LOCATION',
-                icon: Icon(
-                  Icons.add_location_outlined,
-                  color: Colors.black,
-                ),
-                iconMargin: EdgeInsets.only(bottom: 10.0),
-              ),
-              //child: Image.asset('images/android.png'),
+    return MaterialApp(
+      title: 'Tab inside body widget',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
 
-              Tab(
-                text: 'SNOW',
-                icon: Icon(
-                  Icons.ac_unit_rounded,
-                  color: Colors.blue,
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  bool isSwitched = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Tabs Layout'),
+      ),
+      body: Container(
+        child: Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: <Widget>[
+          SizedBox(height: 20.0),
+          Text('Weather Tabs', textAlign: TextAlign.center, style: TextStyle(fontSize: 22)),
+          DefaultTabController(
+              length: 5, // length of tabs
+              initialIndex: 0,
+              child: Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: <Widget>[
+                Container(
+                  child: TabBar(
+                    labelColor: Colors.green,
+                    unselectedLabelColor: Colors.black,
+                    tabs: [
+                      Icon(Icons.ac_unit_rounded),
+                      Icon(Icons.bedtime),
+                      Icon(Icons.brightness_5_outlined),
+                      Icon(Icons.cloud_circle_outlined),
+                      Icon(Icons.beach_access),
+                    ],
+                  ),
                 ),
-                iconMargin: EdgeInsets.only(bottom: 10.0),
-              ),
-              Tab(
-                text: 'DARK',
-                icon: Icon(
-                  Icons.bedtime,
-                  color: Colors.black,
-                ),
-                iconMargin: EdgeInsets.only(bottom: 10.0),
-              ),
-              Tab(
-                text: 'SUNNY',
-                icon: Icon(
-                  Icons.brightness_5_outlined,
-                  color: Colors.deepOrange,
-                ),
-                iconMargin: EdgeInsets.only(bottom: 10.0),
-              ),
-              Tab(
-                text: 'CLOUDY',
-                icon: Icon(
-                  Icons.cloud_circle_outlined,
-                  color: Colors.black,
-                ),
-                iconMargin: EdgeInsets.only(bottom: 10.0),
-              ),
-              Tab(
-                text: 'RAINY',
-                icon: Icon(
-                  Icons.beach_access,
-                  color: Colors.blueGrey,
-                ),
-                iconMargin: EdgeInsets.only(bottom: 10.0),
-              ),
-            ],
+                Container(
+                    height: 400, //height of TabBarView
+                    decoration: BoxDecoration(
+                        border: Border(top: BorderSide(color: Colors.grey, width: 0.5))
+                    ),
+                    child: TabBarView(children: <Widget>[
+                      Container(
+                        child: Center(
+                          child: Switch(
+                            value: isSwitched,
+                            onChanged: (value) {
+                              setState(() {
+                                isSwitched = value;
+                              });
+                            },
+                            activeTrackColor: Colors.lightGreenAccent,
+                            activeColor: Colors.green,
+                          ),
+                          /*Icon(Icons.add_location_outlined,
+                          color: Colors.black,
+                            size: 200,
+                          ),*/
+
+                          //Text('Display Tab 1', style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
+                        ),
+                      ),
+                      Container(
+                        child: Center(
+                          child: Switch(
+                            value: isSwitched,
+                            onChanged: (value) {
+                              setState(() {
+                                isSwitched = value;
+                              });
+                            },
+                            activeTrackColor: Colors.lightGreenAccent,
+                            activeColor: Colors.green,
+                          ),
+                          //Text('Display Tab 2', style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
+                        ),
+                      ),
+                      Container(
+                        child: Center(
+                          child: Switch(
+                            value: isSwitched,
+                            onChanged: (value) {
+                              setState(() {
+                                isSwitched = value;
+                              });
+                            },
+                            activeTrackColor: Colors.lightGreenAccent,
+                            activeColor: Colors.green,
+                          ),
+                          //Text('Display Tab 2', style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
+                        ),
+                      ),
+                      Container(
+                        child: Center(
+                          child: Switch(
+                            value: isSwitched,
+                            onChanged: (value) {
+                              setState(() {
+                                isSwitched = value;
+                              });
+                            },
+                            activeTrackColor: Colors.lightGreenAccent,
+                            activeColor: Colors.green,
+                          ),
+                          //Text('Display Tab 3', style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
+                        ),
+                      ),
+                      Container(
+                        child: Center(
+                          child: Switch(
+                            value: isSwitched,
+                            onChanged: (value) {
+                              setState(() {
+                                isSwitched = value;
+                              });
+                            },
+                            activeTrackColor: Colors.lightGreenAccent,
+                            activeColor: Colors.green,
+                          ),
+                          //Text('Display Tab 4', style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
+                        ),
+                      ),
+                    ])
+                )
+              ])
           ),
-        ),
-        body: TabBarView(
-          children: [
-            Center(
-                child: Text(
-                  'This is Cake Tab',
-                  style: TextStyle(fontSize: 32),
-                )),
-            Center(
-                child: Text(
-                  'This is Radio Tab',
-                  style: TextStyle(fontSize: 32),
-                )),
-            Center(
-                child: Text(
-                  'This is Gift Tab',
-                  style: TextStyle(fontSize: 32),
-                )),
-          ],
-        ),
+        ]),
       ),
     );
   }
